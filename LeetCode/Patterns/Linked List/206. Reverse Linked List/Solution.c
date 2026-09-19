6dcd2a8da7f8fabaf5fc50ca0659074cf27b0171
Kaviya V <121804254+7-Kaviya@users.noreply.github.com>
2026-09-19 17:27:49 +0530

    Add Easy C Solution for 206. Reverse Linked List from LeetCode

---
+++ LeetCode/Patterns/Linked List/206. Reverse Linked List/Solution.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#include <stdlib.h>
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr != NULL) {
+        struct ListNode* nextNode = curr->next; // Save next node
+        curr->next = prev; // Reverse the pointer
+        prev = curr; // Move prev forward
+        curr = nextNode; // Move curr forward
+    }
+    return prev;
+}
